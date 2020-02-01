943be430e3df3825757db0b7e5e03f26839de332
Suraj R Gholap <47596504+surajg1@users.noreply.github.com>
2020-02-01 08:04:34 +0530

    Try to deployee in herokuapp

--- backend-node/models/mongoose-connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://surajgholap:srj@1998@cluster0-kmd7r.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
-            .then(()=>{
-                console.log("C");
-            }).catch(()=>{
-                console.log("E");
-            });
-
-            // srj@9604
--- backend-node/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controller/user")
-
-router.post("/signup", userController.createUser);
-
-router.post("/login", userController.userLogin);
-
-module.exports = router;
